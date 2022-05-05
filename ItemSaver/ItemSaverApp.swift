f56ef7660528fac217f1a3468ace0b0e8580643f
matt collyer <mcollyer@lcs.on.ca>
2022-05-05 09:41:31 -0400

    Initial Commit

---
+++ ItemSaver/ItemSaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItemSaverApp.swift
+//  ItemSaver
+//
+//  Created by Matthew Collyer on 2022-05-05.
+//
+
+import SwiftUI
+
+@main
+struct ItemSaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
